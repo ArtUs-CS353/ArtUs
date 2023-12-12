552856c65f4a906413d2421d1ded195ca4253716
blockerst <goktug.yilmaz@ug.bilkent.edu.tr>
2023-12-12 08:19:42 +0300

    some api points added into artwork and artist controllers

--- backend/artus/src/main/java/com/artus/artus/services/ArtistService.java
+++ backend/artus/src/main/java/com/artus/artus/services/ArtistService.java
@@ -34,4 +34,10 @@ public class ArtistService {
         String sql = "SELECT AU.type AS auction_type,AW.type AS artwork_type, AU.status AS auction_status, AU.*,AW.* FROM Auction AU, Artwork AW, Artist AR where AU.artwork_id = AW.artwork_id AND AW.artist_id = AR.user_id AND AR.user_id = ?;";
         return jdbcTemplate.query(sql,new AuctionPropertiesMapper(),artist_id);
     }
+    public List<Artist> searchArtistByNameBySurname(String search){
+        String mainSQL = "WITH temp (user_name,user_surname,contact_info,balance,biography,is_featuring,follower_count,user_id,profile_details,name_surname) as (SELECT U.user_name,U.user_surname,U.contact_info,A.balance,A.biography,A.is_featuring,A.follower_count,U.user_id,A.profile_details,CONCAT(U.user_name, ' ', U.user_surname) as name_surname FROM Artist A, User U WHERE A.user_id = U.user_id) Select * FROM temp WHERE name_surname LIKE ? ";
+        return jdbcTemplate.query(mainSQL,new ArtistMapper(),"%" + search + "%" );
+
+
+    }
 }
--- backend/artus/src/main/java/com/artus/artus/services/ArtworkService.java
+++ backend/artus/src/main/java/com/artus/artus/services/ArtworkService.java
@@ -3,10 +3,13 @@ package com.artus.artus.services;
 import com.artus.artus.mappers.ArtworkMapper;
 import com.artus.artus.models.Artwork;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ArtworkService {
@@ -72,5 +75,102 @@ public class ArtworkService {
             // If the insertion fails, return false
             return null;
         }
+    }
+
+    public List<Artwork> searchArtworkWithTitle(String title){
+        try{
+            String sql = "SELECT * FROM Artwork WHERE title like ?";
+            return jdbcTemplate.query(sql, artworkMapper, "%" + title + "%");
+        }
+        catch(Exception e){
+            // Handle exceptions, log errors, etc.
+            e.printStackTrace();
+            // If the insertion fails, return false
+            return null;
+        }
+    }
+
+    public List<Artwork> filterArtworks(List<String> types, List<String> materials, List<String> rarities, Integer minPrice, Integer maxPrice, LocalDate startDate, LocalDate endDate){
+        String mainSQL = "SELECT * FROM Artwork WHERE ";
+        List<Object> allParams = new ArrayList<>();
+
+        if(minPrice != null){
+            StringBuilder sqlBuilderForMinPrice = new StringBuilder();
+            sqlBuilderForMinPrice.append(" ( price >= ? ) AND");
+            mainSQL = mainSQL + sqlBuilderForMinPrice;
+            allParams.add(minPrice);}
+
+        if(maxPrice != null){
+            StringBuilder sqlBuilderForMaxPrice = new StringBuilder();
+            sqlBuilderForMaxPrice.append(" ( price <= ? ) AND");
+            mainSQL = mainSQL + sqlBuilderForMaxPrice;
+            allParams.add(maxPrice);
+        }
+
+        if(startDate != null){
+            StringBuilder sqlBuilderForstartDate = new StringBuilder();
+            sqlBuilderForstartDate.append(" ( date >= ? ) AND");
+            mainSQL = mainSQL + sqlBuilderForstartDate;
+            allParams.add(startDate);
+        }
+
+        if(endDate != null){
+            StringBuilder sqlBuilderForEndDate = new StringBuilder();
+            sqlBuilderForEndDate.append(" ( date <= ? ) AND");
+            mainSQL = mainSQL + sqlBuilderForEndDate;
+            allParams.add(endDate);
+        }
+
+        if(materials != null){
+            StringBuilder sqlBuilderforMaterials = new StringBuilder();
+            sqlBuilderforMaterials.append(" ( ");
+            for (int i = 0; i < materials.size(); i++) {
+                if (i > 0) {
+                    sqlBuilderforMaterials.append(" OR ");
+                }
+                sqlBuilderforMaterials.append("material LIKE ?");
+            }
+            sqlBuilderforMaterials.append(" ) ");
+            mainSQL = mainSQL + sqlBuilderforMaterials;
+            mainSQL = mainSQL + " AND ";
+            allParams.addAll(materials.stream().map(material -> "%" + material + "%").collect(Collectors.toList()));
+        }
+        if(types != null){
+            StringBuilder sqlBuilderfortypes = new StringBuilder();
+            sqlBuilderfortypes.append(" ( ");
+            for (int i = 0; i < types.size(); i++) {
+                if (i > 0) {
+                    sqlBuilderfortypes.append(" OR ");
+                }
+                sqlBuilderfortypes.append("type LIKE ?");
+            }
+            sqlBuilderfortypes.append(" ) ");
+            mainSQL = mainSQL + sqlBuilderfortypes;
+            mainSQL = mainSQL + " AND ";
+            allParams.addAll(types.stream().map(type -> "%" + type + "%").collect(Collectors.toList()));
+        }
+        if(rarities != null){
+            StringBuilder sqlBuilderforRarities = new StringBuilder();
+            sqlBuilderforRarities.append(" ( ");
+            for (int i = 0; i < rarities.size(); i++) {
+                if (i > 0) {
+                    sqlBuilderforRarities.append(" OR ");
+                }
+                sqlBuilderforRarities.append("rarity LIKE ?");
+            }
+            sqlBuilderforRarities.append(" ) ");
+            mainSQL = mainSQL + sqlBuilderforRarities;
+            mainSQL = mainSQL + " AND ";
+            allParams.addAll(rarities.stream().map(rarity -> "%" + rarity + "%").collect(Collectors.toList()));
+        }
+        mainSQL = mainSQL + " true ";
+
+        return jdbcTemplate.query(mainSQL,artworkMapper,allParams.toArray());
+
+    }
+
+    public List<Artwork> getExplorePage(){
+        String sql = "select * from most_favorite_artworks;";
+        return jdbcTemplate.query(sql,new ArtworkMapper());
     }
 }
