828b19ffa6f5e578d7bdb20e6dc9ec4497ba1d76
blockerst <goktug.yilmaz@ug.bilkent.edu.tr>
2023-12-18 22:30:11 +0300

    enthusiast controller changes

--- backend/artus/src/main/java/com/artus/artus/mappers/EnthusiastMapper.java
+++ backend/artus/src/main/java/com/artus/artus/mappers/EnthusiastMapper.java
@@ -15,9 +15,7 @@ public class EnthusiastMapper implements RowMapper<Enthusiast> {
         String user_name = rs.getString("user_name");
         String user_surname = rs.getString("user_surname");
         int user_role = rs.getInt("user_role");
-        String email = rs.getString("email");
-        String password = rs.getString("password");
-        String contact_info = rs.getString("contact_info");
+
 
         Enthusiast enthusiast = new Enthusiast();
         enthusiast.setUser_id(user_id);
@@ -26,9 +24,7 @@ public class EnthusiastMapper implements RowMapper<Enthusiast> {
         enthusiast.setUser_name(user_name);
         enthusiast.setUser_surname(user_surname);
         enthusiast.setUser_role(user_role);
-        enthusiast.setEmail(email);
-        enthusiast.setPassword(password);
-        enthusiast.setContact_info(contact_info);
+
 
         return enthusiast;
     }
